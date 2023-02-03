379061fb493574c49bf88e02ec3d81ec4a1dafc1
unknown <dushyantsirohi100@gmail.com>
2023-02-03 14:29:55 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './CustomDirective/highlight.directive';
+import { SetBackgroundDirective } from './CustomDirective/setbackground.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetBackgroundDirective,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
